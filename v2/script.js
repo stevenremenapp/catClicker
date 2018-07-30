4c416c7ac1cd52e8836b0d0868870e560c6ea399
stevenremenapp <remenapp@gmail.com>
2018-07-29 23:47:41 -0400

    Get cat click functioning working using OOJS principles!

--- v2/script.js
+++ v2/script.js
@@ -1,37 +1,70 @@
-catImages = [
-    {
+cats = {
+    1: {
         name: 'Xuxa',
-        src: 'cat1.jpg'
+        src: 'cat1.jpg',
+        clicks: 0
     },
-    {
+    2: {
         name: 'Ferdinand',
-        src: 'cat2.jpg'
+        src: 'cat2.jpg',
+        clicks: 0
     }
-]
+}
 
 // Set cat names
 
-let catNames = [];
-let catNameDisplays = document.querySelectorAll('.catName');
-catNames.push(catNameDisplays);
+let catDivs = document.querySelectorAll('.cat');
+console.log(catDivs);
 
-for (let i = 0; i < catNames[0].length; i++) {
-    let catNameDisplay = catNames[0][i];
-    catNameDisplay.textContent = catImages[i].name;
+for (let i = 0; i < catDivs.length; i++) {
+    let catName = document.createElement('p');
+    catName.setAttribute('class', 'catName');
+    catDivs[i].prepend(catName);
+    // catName.textContent = cats[i].name;
+    // for (j = 0; j < cats.length; j++) {
+    //     catName.textContent = cats[i][j].name;
+    // }
+    
 }
 
 
-// let cat1Name = document.getElementById('cat1Name');
-// cat1Name.textContent = catImages[0].name;
+// Set event listener to count clicks on images
 
-// let cat2Name = document.getElementById('cat2Name');
-// cat2Name.textContent = catImages[1].name;
+let catClicker = document.querySelector('.catClicker');
 
+catClicker.addEventListener('click', function(event) {
+    if (event.target.tagName === 'IMG') {
+        console.log(event.target);
+        let catClicked = event.target.dataset.cat;
+        console.log(catClicked);
+        let catDivClicked = event.target.parentNode;
+        // let catScore = cats[catClicked].clicks;
+        // catScore += 1;
+        cats[catClicked].clicks += 1;
+        console.log(cats[catClicked].clicks);
 
-let catImage = document.getElementById('catImage');
-let clickCount = 0;
-cat1Image.addEventListener('click', function() {
-    let clickCountDisplay = document.getElementById('cat1ClickCount');
-    clickCount++;
-    clickCountDisplay.textContent = clickCount;
+            if (catDivClicked.classList.contains('scoreAdded')) {
+                let currentClickDisplay = catDivClicked.querySelector('.currentClicks');
+                console.log(currentClickDisplay);
+                currentClickDisplay.textContent = cats[catClicked].clicks;
+            } else {
+                let newClickDisplay = document.createElement('span');
+                // newClickDisplay.innerHTML = "Number of cat clicks: <span class = 'currentClicks'></span>";
+                newClickDisplay.setAttribute('class', 'currentClicks');
+                catDivClicked.appendChild(newClickDisplay);
+                newClickDisplay.textContent = cats[catClicked].clicks;
+                catDivClicked.classList.add('scoreAdded');
+            }            
+        // }
+    }
 });
+
+
+// let cat1Image = document.getElementById('cat1Image');
+// let clickCount = 0;
+
+// cat1Image.addEventListener('click', function() {
+//     let clickCountDisplay = document.getElementById('cat1ClickCount');
+//     clickCount++;
+//     clickCountDisplay.textContent = clickCount;
+// });
