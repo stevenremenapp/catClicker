2b3d05a0d7c643585107885a4009cf15f713fec2
stevenremenapp <remenapp@gmail.com>
2018-07-30 00:24:42 -0400

    Make cat name display dynamic using OOJS principles

--- v2/script.js
+++ v2/script.js
@@ -11,48 +11,46 @@ cats = {
     }
 }
 
-// Set cat names
+// Global Selectors
 
+let catClicker = document.querySelector('.catClicker');
 let catDivs = document.querySelectorAll('.cat');
 console.log(catDivs);
+
+// Set cat names
 
 for (let i = 0; i < catDivs.length; i++) {
     let catName = document.createElement('p');
     catName.setAttribute('class', 'catName');
     catDivs[i].prepend(catName);
-    // catName.textContent = cats[i].name;
-    // for (j = 0; j < cats.length; j++) {
-    //     catName.textContent = cats[i][j].name;
-    // }
-    
+    let cat = catDivs[i].dataset.cat;
+    catName.textContent = cats[cat].name;    
 }
 
 
 // Set event listener to count clicks on images
 
-let catClicker = document.querySelector('.catClicker');
-
 catClicker.addEventListener('click', function(event) {
     if (event.target.tagName === 'IMG') {
         console.log(event.target);
-        let catClicked = event.target.dataset.cat;
-        console.log(catClicked);
+        let catClickedId = event.target.parentNode.dataset.cat;
+        console.log(catClickedId);
         let catDivClicked = event.target.parentNode;
-        // let catScore = cats[catClicked].clicks;
+        // let catScore = cats[catClickedId].clicks;
         // catScore += 1;
-        cats[catClicked].clicks += 1;
-        console.log(cats[catClicked].clicks);
+        cats[catClickedId].clicks += 1;
+        console.log(cats[catClickedId].clicks);
 
             if (catDivClicked.classList.contains('scoreAdded')) {
                 let currentClickDisplay = catDivClicked.querySelector('.currentClicks');
                 console.log(currentClickDisplay);
-                currentClickDisplay.textContent = cats[catClicked].clicks;
+                currentClickDisplay.textContent = cats[catClickedId].clicks;
             } else {
                 let newClickDisplay = document.createElement('span');
                 // newClickDisplay.innerHTML = "Number of cat clicks: <span class = 'currentClicks'></span>";
                 newClickDisplay.setAttribute('class', 'currentClicks');
                 catDivClicked.appendChild(newClickDisplay);
-                newClickDisplay.textContent = cats[catClicked].clicks;
+                newClickDisplay.textContent = cats[catClickedId].clicks;
                 catDivClicked.classList.add('scoreAdded');
             }            
         // }
