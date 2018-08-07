3fc4e5134700fe841d93f294a5470658078c26a5
stevenremenapp <remenapp@gmail.com>
2018-08-07 15:50:52 -0400

    Work through solution code to understand it

--- v4-premium-pro/script.js
+++ v4-premium-pro/script.js
@@ -1,41 +1,134 @@
-cats = {
-    1: {
-        name: 'Xuxa',
-        src: 'img/cat1.jpg',
-        alt: 'Adorable kitten with tan and brown fur staring into the camera',
-        clicks: 0
-    },
-    2: {
-        name: 'Ferdinand',
-        src: 'img/cat2.jpg',
-        alt: 'Adorable kitten with white and brown fur and blue eyes staring into the camera from behind a stairpost',
-        clicks: 0
-    },
-    3: {
-        name: 'Truman',
-        src: 'img/cat3.jpg',
-        alt: 'The head and open mouth of a black cat in the middle of a yawn revealing teeth and tongue',
-        clicks: 0
-    },
-    4: {
-        name: 'Suman',
-        src: 'img/cat4.jpg',
-        alt: 'A black and white cat laying with its arms outstretched on top of a small bench which is perched atop a beige reclining chair',
-        clicks: 0
-    },
-    5: {
-        name: 'Goose',
-        src: 'img/cat5.jpg',
-        alt: 'A fluffy tan black and white cat standing on its hind legs while a human scratches its belly',
-        clicks: 0
-    },
-    6: {
-        name: 'Evie',
-        src: 'img/cat6.jpg',
-        alt: 'A small tan brown and white cat curled around on its back with its head upside down and paws in the air while looking back at the camera ',
-        clicks: 0
-    }
+//  -------  MODEL  ------- //
+
+let data = {
+    currentCat: null,
+    cats: [
+        {
+            name: 'Xuxa',
+            src: 'img/cat1.jpg',
+            alt: 'Adorable kitten with tan and brown fur staring into the camera',
+            clicks: 0
+        },
+        {
+            name: 'Ferdinand',
+            src: 'img/cat2.jpg',
+            alt: 'Adorable kitten with white and brown fur and blue eyes staring into the camera from behind a stairpost',
+            clicks: 0
+        },
+        {
+            name: 'Truman',
+            src: 'img/cat3.jpg',
+            alt: 'The head and open mouth of a black cat in the middle of a yawn revealing teeth and tongue',
+            clicks: 0
+        },
+        {
+            name: 'Suman',
+            src: 'img/cat4.jpg',
+            alt: 'A black and white cat laying with its arms outstretched on top of a small bench which is perched atop a beige reclining chair',
+            clicks: 0
+        },
+        {
+            name: 'Goose',
+            src: 'img/cat5.jpg',
+            alt: 'A fluffy tan black and white cat standing on its hind legs while a human scratches its belly',
+            clicks: 0
+        },
+        {
+            name: 'Evie',
+            src: 'img/cat6.jpg',
+            alt: 'A small tan brown and white cat curled around on its back with its head upside down and paws in the air while looking back at the camera ',
+            clicks: 0
+        }
+    ]
 }
+
+//  -------  CONTROLLER & VIEW  ------- //
+
+let methods = {
+
+    init: function() {
+        // Init cat div
+        this.catDiv = document.getElementById('cat');
+        this.catName = document.getElementById('catName');
+        this.catImage = document.getElementById('catImage');
+        this.catClickCount = document.getElementById('clickCount');
+
+        this.catImage.addEventListener('click', function() {
+            methods.incrementClickCount();
+        });
+
+        // Init cat list
+        this.catList = document.getElementById('catList');
+
+        // render the DOM according to these values
+        this.render();
+    },
+
+    getCats: function() {
+        return data.cats;
+    },
+
+    getCurrentCat: function() {
+        return data.currentCat;
+    },
+
+    setCurrentCat: function(cat) {
+        data.currentCat = cat;
+    },
+
+    incrementClickCount: function() {
+        data.currentCat.clicks += 1;
+        methods.render();
+    },
+
+    // Method for creating the cat nav list
+    createCatNav: function() {
+    
+    },
+    // Method for updating clickCount
+
+    // Method for setting current cat and displaying the cat
+
+    // Render the view
+    render: function() {
+        // Render the cat div
+        let currentCat = methods.getCurrentCat();
+        this.catName = currentCat.name;
+        this.catImage = currentCat.src;
+        this.catClickCount = currentCat.clicks;
+
+        // Render the cat list
+        let cat, listItem, i;
+
+        let cats = methods.getCats();
+
+        this.catList.innerHTML = '';
+
+        for (i = 0; i < cats.length; i++) {
+            cat = cats[i];
+            listItem = document.createElement('li');
+            listItem.textContent = cat.name;
+
+            listItem.addEventListener('click', (function(catCopy) {
+                return function() {
+                    methods.setCurrentCat(catCopy);
+                    methods.render();
+                };
+            })(cat));
+        }
+
+        this.catList.appendChild(listItem);
+    }
+};
+
+// Launch Cat Clicker!
+methods.init();
+
+
+
+
+
+/*
 
 // Global Selectors
 let catClicker = document.querySelector('.catClicker');
@@ -125,3 +218,5 @@ catClicker.addEventListener('click', function(event) {
         currentClickDisplay.textContent = cats[catClickedId].clicks;           
     }
 });
+
+*/
